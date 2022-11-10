6d1e78e2d8dae21a719abb4799263ffae3446434
sai-k02 <srilokh.karuturi@gmail.com>
2022-11-10 01:23:08 -0600

    iterate services

---
+++ Client/README.md
@@ -0,0 +1,3 @@
+https://www.microsoft.com/design/fluent/
+https://evergreen.segment.com/components/spinner
+https://rsuitejs.com/components/auto-complete/
---
+++ Server/src/routes/bookAuthors.route.ts
@@ -0,0 +1,31 @@
+// router.get("/:id", getbookAuthors)
+// router.post("/", createPost);
+// router.patch("/:id", updatePost);
+// router.delete("/:id", deletePost);
+
+// IMPORT EXPRESS
+import express from "express";
+
+// DEFINE ROUTER
+const bookAuthorRouter = express.Router();
+
+// IMPORT SERVICES
+import {
+    createBookAuthor,
+    getAllBookAuthors,
+    getBookAuthor,
+    deleteBookAuthor,
+    updateBookAuthor
+} from "../services/bookAuthors.service";
+
+console.log("[server] Routing bookAuthor Request")
+
+// DEFINE ROUTES
+bookAuthorRouter.get("/", getAllBookAuthors);
+bookAuthorRouter.get("/:id", getBookAuthor);
+bookAuthorRouter.post("/", createBookAuthor);
+bookAuthorRouter.delete("/:id", deleteBookAuthor)
+bookAuthorRouter.put("/:id", updateBookAuthor)
+
+// EXPORT ROUTER 
+export default bookAuthorRouter;
