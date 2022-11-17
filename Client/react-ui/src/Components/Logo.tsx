891085d9768a76dfbd2152450a774760af08fab0
Minh Nguyen <nhaatmnguyeen@gmail.com>
2022-11-17 00:32:55 -0600

    Add library logo

--- Client/react-ui/src/Components/Logo.tsx
+++ Client/react-ui/src/Components/Logo.tsx
@@ -1,12 +1,22 @@
 import React from "react"
-import logo from './../logo.svg'
 import { Button, Icon, IconBookOpen } from '@supabase/ui'
 import { useNavigate } from "react-router-dom"
+
+
 export default function Logo() {
     let navigate = useNavigate();
     return (
-        <div className="p-2">
-            <Button onClick={() => { navigate("/") }} type="default" shadow={true} size={"medium"} iconRight={<IconBookOpen />}>Library</Button>
+        <div className="flex space-x-2 flex items-stretch">
+            <div>
+                <Button style={{ "fontWeight": "bold", "color": "black" }} onClick={() => { navigate("/") }} type="text" size={"large"} icon={<IconBookOpen />} >
+                    Library
+                </Button>
+            </div>
+            {/* <body className="relative">
+                <div className="absolute left-1/2 ml-1 w-0.5 h-full bg-slate-600">
+                </div>
+            </body> */}
         </div>
     )
 }
+
