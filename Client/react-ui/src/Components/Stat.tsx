460095e88e893c68d5463e4c152d4df702078276
sai-k02 <srilokh.karuturi@gmail.com>
2022-11-17 01:33:44 -0600

    Add Stat Functionality

--- Client/react-ui/src/Components/Stat.tsx
+++ Client/react-ui/src/Components/Stat.tsx
@@ -1,7 +1,23 @@
 import react from "react"
 
+type StatCardProps = {
+    Name?: String,
+    Amount?: String,
+    Loading?: String
+}
 
-export default function StatCard(){
+export default function StatCard(props: StatCardProps) {
+    if (props.Amount) {
+        return (
+            <div className="relative w-80 h-80 bg-[#6D696A] rounded-lg">
+                <div className="text-white font-semibold p-2">
+                    {props.Amount}
+                </div>
+            </div>
+        )
+    }
+
+
     return (
         <div className="relative w-80 h-80 bg-[#6D696A] rounded-lg">
             <div className="text-white font-semibold p-2">
--- Client/react-ui/src/Home.tsx
+++ Client/react-ui/src/Home.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Logo from "./Components/Logo"
-import { Card } from '@supabase/ui';
+import { Button, Card } from '@supabase/ui';
+import { useQuery } from 'react-query'
 import Search from './Components/Search';
 import StatCard from './Components/Stat';
 export default function Home() {
+    const [amountBooks, setAmountBooks] = React.useState<{}>();
+    useEffect(() => {
+
+        getNumBooks().then(
+            (response: {}) => {
+                if (response) {
+                    //@ts-ignore
+                    setAmountBooks(response.Amount)
+                }
+            })
+    }, []);
     return (
         <div className="">
             <Header />
@@ -14,13 +26,13 @@ export default function Home() {
             <div className="bg-[#d9d9d9] w-screen h-screen">
                 <div className={"flex flex-col items-center justify-center "} >
                     <div className="m-8">
-                    <Search />
+                        <Search />
                     </div>
                     <div className={"flex space-x-10 m-2"}>
-                    <StatCard />
-                    <StatCard />
-                    <StatCard />
-                    <StatCard />
+                        <StatCard Amount={String(amountBooks)} />
+                        <StatCard />
+                        <StatCard />
+                        <StatCard />
                     </div>
                     <div className="flex justify-center mt-10 ">
                         <h1 className="absolute font-bold text-4xl "> Team Beriyllium </h1>
@@ -39,8 +51,16 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-
+            <Button onClick={() => getNumBooks()}></Button>
             <Footer />
         </div>
     )
-} 
+}
+
+
+const getNumBooks = async () => {
+    const response = await fetch("http://localhost:3001/book/?onlyMeta=True")
+    const responseJSON = JSON.parse(await response.text())
+
+    return responseJSON
+}
