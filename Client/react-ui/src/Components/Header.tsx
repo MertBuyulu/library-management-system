42f41b1fcfae01bece370910fae8aab765cfa827
Minh Nguyen <nhaatmnguyeen@gmail.com>
2022-11-17 00:31:44 -0600

    Add tabs and plus symbol

--- Client/react-ui/src/Components/Header.tsx
+++ Client/react-ui/src/Components/Header.tsx
@@ -1,28 +1,32 @@
 import React from "react"
 import Logo from './Logo';
-import { Button, Icon, IconBookOpen } from '@supabase/ui'
+import { Button, Icon, IconBookOpen, IconPlusCircle } from '@supabase/ui'
 import { useNavigate } from "react-router-dom"
 
 
-
 export default function Header() {
+    // SET 
+
+
     // DEFINE NAVIGATE
     let navigate = useNavigate();
 
     return (
-        <div className="border-2 border-b-black-500 ">
-            < header className="flex justify-between">
+        <div className="border-2 border-b-black-500 bg-[#d9d9d9]">
+            <div>
+            <header className="flex justify-between">
                 <Logo />
-                <div className="flex  space-x-2 p-2 max-sm:space-x-1 max-sm:hidden">
-                    <Button type="text" onClick={() => navigate("/Books")} > Books </Button>
-                    <Button type="text" onClick={() => navigate("/Authors")}> Authors </Button>
-                    <Button type="text" onClick={() => navigate("/Borrowers")}> Borrowers </Button>
-                    <Button type="text" onClick={() => navigate("/Fines")}> Fines </Button>
+                <div className="space-x-3 mt-3 pr-12 ">
+                    <button className="hover:text-[#6D696A]"  onClick={() => navigate("/Books")} > Books </button>
+                    <button className="hover:text-[#6D696A]" onClick={() => navigate("/Authors")}> Authors </button>
+                    <button className="hover:text-[#6D696A]" onClick={() => navigate("/Borrowers")}> Borrowers </button>
+                    <button className="hover:text-[#6D696A]" onClick={() => navigate("/Fines")}> Fines </button>
                 </div>
-
-            </header >
-
-
-        </div >
+                <button className="object-right ">
+                    <svg className="w-11 h-11 pr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path></svg>
+                </button>
+            </header>
+            </div>
+        </div>
     )
 }
