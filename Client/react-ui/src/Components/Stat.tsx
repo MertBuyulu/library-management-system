ae88154d65e2cf31c01cc5a2d3591ba578db5c94
Minh Nguyen <nhaatmnguyeen@gmail.com>
2022-11-17 00:37:16 -0600

    Add stat card content

---
+++ Client/react-ui/src/Components/Stat.tsx
@@ -0,0 +1,12 @@
+import react from "react"
+
+
+export default function StatCard(){
+    return (
+        <div className="relative w-80 h-80 bg-[#6D696A] rounded-lg">
+            <div className="text-white font-semibold p-2">
+                Books
+            </div>
+        </div>
+    )
+}
