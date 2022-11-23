15f816f065dc79bc5611788f7dce439cc5210a09
Minh Nguyen <nhaatmnguyeen@gmail.com>
2022-11-22 22:37:07 -0600

    Add Validate by ISBN const

--- Client/src/utils/utils.js
+++ Client/src/utils/utils.js
@@ -1,4 +1,15 @@
+import { fetchBooksById} from "../api/books";
 import { fetchBorrowerById } from "../api/borrowers";
+
+export const validateIsbn = async (isbn) => {
+  try {
+    const response = await fetchBooksById(isbn);
+    const { data } = response;
+    return Object.keys(data).length === 0;
+  } catch (error) {
+    return error.message;
+  }
+};
 
 export const validateSsn = async (ssn) => {
   try {
